e681f69c9cc1d731fb1d3656eb9d55859e7c035c
anwachuku15 <anwachuku15@gmail.com>
2022-06-04 19:03:06 -0400

    auth screens

---
+++ screens/Onboarding/SignUp.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import SignupForm from "../../components/organisms/SignupForm";
+
+const SignUp = () => {
+	return (
+		<View>
+			<SignupForm />
+		</View>
+	);
+};
+
+export default SignUp;

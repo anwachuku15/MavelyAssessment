4654f740e8a08aa02cedaebd4aad1696c11c8bb8
anwachuku15 <anwachuku15@gmail.com>
2022-06-04 19:03:37 -0400

    created Onboarding navigator

---
+++ navigation/OnboardingNavigator.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { View, Dimensions } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import {
+	createStackNavigator,
+	TransitionPresets,
+} from "@react-navigation/stack";
+import Welcome from "../screens/Onboarding/Welcome";
+import Login from "../screens/Onboarding/Login";
+import SignUp from "../screens/Onboarding/SignUp";
+
+const screenHeight = Dimensions.get("screen").height;
+
+const OnboardingNavigator = () => {
+	const navigation = useNavigation();
+	const OnboardingStack = createStackNavigator();
+
+	return (
+		<OnboardingStack.Navigator screenOptions={() => ({ headerShown: false })}>
+			<OnboardingStack.Screen
+				name="Welcome"
+				component={Welcome}
+				options={{ headerShown: false }}
+			/>
+			<OnboardingStack.Screen
+				name="Login"
+				component={Login}
+				options={() => ({
+					...TransitionPresets.ModalPresentationIOS,
+					// TODO: Conditional gesture based on ScrollView
+					gestureResponseDistance: screenHeight,
+					cardStyle: {
+						backgroundColor: "black",
+					},
+				})}
+			/>
+			<OnboardingStack.Screen
+				name="SignUp"
+				component={SignUp}
+				options={() => ({
+					...TransitionPresets.ModalPresentationIOS,
+					// TODO: Conditional gesture based on ScrollView
+					gestureResponseDistance: screenHeight,
+					cardStyle: {
+						backgroundColor: "black",
+					},
+				})}
+			/>
+		</OnboardingStack.Navigator>
+	);
+};
+
+export default OnboardingNavigator;

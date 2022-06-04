051e68220c3786bcbf370e3ec1d1a26a25a80daf
anwachuku15 <anwachuku15@gmail.com>
2022-06-04 19:02:51 -0400

    touchable component

---
+++ components/atoms/TouchableCmp.js
@@ -0,0 +1,12 @@
+import {
+	Platform,
+	TouchableOpacity,
+	TouchableNativeFeedback,
+} from "react-native";
+
+let TouchableCmp = TouchableOpacity;
+if (Platform.OS === "android" && Platform.Version >= 21) {
+	TouchableCmp = TouchableNativeFeedback;
+}
+
+export default TouchableCmp;

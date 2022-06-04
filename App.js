b08099321d2aecc412593daf3625638cd8988fa3
anwachuku15 <anwachuku15@gmail.com>
2022-06-04 19:03:54 -0400

    added apollo provider and navigators

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallBack, useEffect, useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Appearance } from "react-native";
+import TouchableCmp from "./components/atoms/TouchableCmp";
+import * as SplashScreen from "expo-splash-screen";
+
+import { NavigationContainer } from "@react-navigation/native";
+import {
+	createStackNavigator,
+	TransitionPresets,
+} from "@react-navigation/stack";
+
+import {
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+	createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { getProducts } from "./graphql/queries";
+import Welcome from "./screens/Onboarding/Welcome";
+import OnboardingNavigator from "./navigation/OnboardingNavigator";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	// TODO: Set up Apollo Client
+	const client = new ApolloClient({
+		uri: "https://mavely.top",
+		cache: new InMemoryCache(),
+	});
+
+	// const httpLink = createHttpLink({
+	//   uri: '/graphql'
+	// })
+	// const authLink = setContext((_, {headers}) => {
+
+	// })
+
+	const [appIsReady, setAppIsReady] = useState(false);
+
+	const RootStack = createStackNavigator();
+
+	return (
+		<ApolloProvider client={client}>
+			<NavigationContainer>
+				<OnboardingNavigator />
+			</NavigationContainer>
+			{/* <View style={styles.container}>
+				<Welcome />
+				<StatusBar style="light" />
+			</View> */}
+		</ApolloProvider>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });

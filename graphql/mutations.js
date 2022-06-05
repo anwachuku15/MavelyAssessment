a8d35ab5e44b2092485f331a9fd0c4fc21731494
anwachuku15 <anwachuku15@gmail.com>
2022-06-05 17:02:34 -0400

    graphql auth mutations

--- graphql/mutations.js
+++ graphql/mutations.js
@@ -1,1 +1,25 @@
 import { gql } from "@apollo/client";
+
+export const SIGN_UP = gql`
+	mutation Signup($data: SignupInput!) {
+		signup(data: $data) {
+			email
+			name
+			id
+			token
+			refreshToken
+		}
+	}
+`;
+
+export const LOG_IN = gql`
+	mutation Login($data: LoginInput!) {
+		login(data: $data) {
+			id
+			email
+			name
+			refreshToken
+			token
+		}
+	}
+`;
